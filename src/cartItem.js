cc67c5572a2a4f0d797a45f6894c364904127955
ritek20mungali <shikha.m@antino.io>
2023-08-27 23:00:31 +0530

    hello new commit

--- src/cartItem.js
+++ src/cartItem.js
@@ -4,16 +4,16 @@ class CartItem extends React.Component {
     render() {
         return (
             < div className="cart-item">
-            <div className="left-block">
-                <img style={styles.image} />
-            </div>
-            < div className="right-block">
-                <div style={{fontSize:20}}>Phone</div>
-                    <div style={{ color:'#777' }}>Rs 999</div>
+                <div className="left-block">
+                    <img style={styles.image} />
+                </div>
+                < div className="right-block">
+                    <div style={{ fontSize: 20 }}>Phone</div>
+                    <div style={{ color: '#777' }}>Rs 999</div>
                     <div style={{ fontSize: 15, color: '#777' }}>QTY. 1</div>
-                <div className="cart-action-items">
-                    {/*button*/}</div>
-            </div>
+                    <div className="cart-action-items">
+                        {/*button*/}</div>
+                </div>
             </div>
         );
     }
@@ -24,7 +24,7 @@ const styles = {
         height: 110,
         width: 110,
         borderRadius: 4,
-        background:'#ccc'
+        background: '#ccc'
     }
 }
 export default CartItem;
